28dac720743cbce3211d23214e1df3619408292e
LuigiMacarini <Luigimacarini1900@gmail.com>
2024-03-20 08:10:54 -0300

    teste commit

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Routes from './src/routes'
 
+//teste
+
 export default function App() {
   return (
     <NavigationContainer>
